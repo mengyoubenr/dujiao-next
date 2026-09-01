968946470119cdfdc38a3b403be9e4ded577e80a
mengyoubenr <133231817+mengyoubenr@users.noreply.github.com>
2026-09-02 00:38:11 +0800

    修复商品名称显示
    修复订单中商品名称里显示实际商品名字，将其改为显示订单号

--- internal/modules/payment/application/payment_service_rules.go
+++ internal/modules/payment/application/payment_service_rules.go
@@ -233,18 +233,6 @@ func buildOrderSubject(order *orderdomain.Order) string {
 	if order == nil {
 		return ""
 	}
-	for i := range order.Items {
-		if title := pickOrderItemTitle(order.Items[i].TitleJSON); title != "" {
-			return title
-		}
-	}
-	for i := range order.Children {
-		for j := range order.Children[i].Items {
-			if title := pickOrderItemTitle(order.Children[i].Items[j].TitleJSON); title != "" {
-				return title
-			}
-		}
-	}
 	return strings.TrimSpace(order.OrderNo)
 }
 
